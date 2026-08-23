8de09bf6430e0d75a7ac3b5650163b57f22e2f91
matviinizovets-dotcom <matvii.nizovets@gmail.com>
2026-08-23 17:45:49 +0300

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const rootDir = path.join(__dirname);
-
-app.use(express.static(rootDir, { index: 'index.html' }));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'index.html'));
-});
